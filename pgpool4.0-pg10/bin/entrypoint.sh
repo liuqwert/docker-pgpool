75784d776a74642c869b156e6e0b3e02590a5793
15251908@qq.com <15251908@qq.com>
2018-11-03 08:00:54 +0800

    update user

--- pgpool4.0-pg10/bin/entrypoint.sh
+++ pgpool4.0-pg10/bin/entrypoint.sh
@@ -7,11 +7,8 @@ export HBA_FILE='/etc/pgpool-II/pool_hba.conf'
 export POOL_PASSWD_FILE='/etc/pgpool-II/pool_passwd'
 
 
-echo '>>> STARTING SSH (if required)...'
-source /home/postgres/.ssh/entrypoint.sh
-
 echo '>>> TURNING PGPOOL...'
-/usr/local/bin/pgpool/pgpool_setup.sh
+/pgpool_setup.sh
 
 echo '>>> STARTING PGPOOL...'
-gosu postgres /usr/local/bin/pgpool/pgpool_start.sh
+/pgpool_start.sh
